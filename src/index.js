7a71ef9c447a965deb56d247d1bbd761562fcbd9
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-07-12 12:59:43 +0200

    Redoing project with MVC

--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
-import './style.css';
-import { makeHeader as header } from './header/renderHeader.js';
-import { navigation } from './navigation/renderNavigation';
-import { main } from './main/renderMain.js';
-import { handlers } from './handlers.js';
-
-let projects = [];
-
-//SOMEWHERE ELSE
-const createNavSection = document.createElement('div');
-createNavSection.classList.add('nav-section');
-
-const createHeaderSection = document.createElement('div');
-createHeaderSection.classList.add('header-section');
-
-const createMainSection = document.createElement('div');
-createMainSection.classList.add('main-section');
-
-//SOMEWHERE ELSE
-function renderLayout() {
-  document.body.append(createNavSection, createHeaderSection, createMainSection);
-  navigation();
-  header();
-  main();
-}
-
-renderLayout();
-handlers.getProject();
 
 
-export {
-  projects,
-  createNavSection,
-  createHeaderSection,
-  createMainSection,
-}
 
 
 

759629789738c0d1f9630bc9d0797c997ab3ac90
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-07-18 10:23:36 +0200

    Adjusted few things

--- src/model.js
+++ src/model.js
@@ -29,30 +29,56 @@ export class Model {
 
     this.projects = [];
 
-    this.allProjects = this.defaultProjects.concat(this.projects);
+    this.allProjects = this.defaultProjects;
   }
 
   addProject(projectTitle) {
     const project = {
-      id: this.projects.length > 0 ? this.projects[this.projects.length - 1].id + 1 : 1,
+      id: this.projects.length > 0 ? this.projects[this.projects.length - 1].id + 1 : 5,
       title: projectTitle,
       todos: [],
       isActive: true,
     }
 
+    this.changeActivePage();
     this.projects.push(project);
+    this.updateAllProjects();
 
     this.onProjectListChanged(this.projects);
   }
 
   returnProjects() { // DELETE LATER
+    console.log('Projects: ');
+    console.log(this.projects);
+    console.log('All Projects: ');
     console.log(this.allProjects); 
   }
+
+  updateAllProjects() {
+    this.allProjects = [...this.defaultProjects, ...this.projects];
+  }
+
+  changeActivePage() {
+    for (let i = 0; i < this.allProjects.length; i++) {
+      if (this.allProjects[i].isActive === true) {
+        this.allProjects[i].isActive = false;
+      }
+    }
+  }
+
+  // FIND HOW TO SOLVE THIS IF LAST PROJECT IS SELECTED AND DELETED isActive will be changed to first element from defaultProject
+  // getDefaultPage(id) {
+  //     console.log(id);
+  //     console.log(this.allProjects.length - 1);
+  // }
 
   deleteProject(id) {
     this.projects = this.projects.filter((project) => project.id !== id);
 
     this.onProjectListChanged(this.projects);
+    this.updateAllProjects();
+
+    //this.getDefaultPage(id);
   }
 
   bindProjectListChanged(callback) {
